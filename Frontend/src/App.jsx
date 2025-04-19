d70a07c13a1c226df75cf3901928b586899edc78
Raff_12 <raffelecoppola381@gmail.com>
2025-04-19 19:20:26 +0200

    feat: Implementazione dello slider e click dei rispettivi brani

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,6 +6,7 @@ import PlayerContextProvider from './assets/context/PlayerContext'
 
 function App() {
  // Route Principale per la gestione delle route figle da "IndexHome".
+ // 4. Dopo aver creato il pattern si utilizza il componente creato al padre dove provede di esportarlo su tutti i suoi figli.
   return (
     <BrowserRouter>
       <PlayerContextProvider>
--- Frontend/src/assets/context/PlayerContext.jsx
+++ Frontend/src/assets/context/PlayerContext.jsx
@@ -2,11 +2,13 @@ import React, { createContext, useEffect, useState } from 'react'
 import { useRef } from 'react';
 import { songsData } from '../js/assets';
 
-export const PlayerContext = createContext();
+// utilizzo del Pattern Provider nel espotazione dati:
+
+export const PlayerContext = createContext(); // 1. Creazione di una CreateContext
 
 export default function PlayerContextProvider(props) {
 
-    const audioRef = useRef();
+    const audioRef = useRef(); // Prende in riferimento l'elemento html del componente.
     const seekBg = useRef();
     const seekBar = useRef();
 
@@ -27,7 +29,7 @@ export default function PlayerContextProvider(props) {
         setTimeout(() => {
             audioRef.current.ontimeupdate = () =>{
             // console.log(seekBar.current.style);
-            seekBar.current.style.width = `${(Math.floor(audioRef.current.currentTime / audioRef.current.duration * 100))}px`;
+            seekBar.current.style.width = `${(Math.floor(audioRef.current.currentTime / audioRef.current.duration * 100))}%`;
                 setTime({
                     currectTime:{
                         second: Math.floor(audioRef.current.currentTime % 60),
@@ -51,17 +53,46 @@ export default function PlayerContextProvider(props) {
         audioRef.current.pause();
         setPlayStaus(false);
     }
+
+    const playWithId = async (id) =>{
+        const song_id = await setTrack(songsData[id]);
+        const audio_ref = await audioRef.current.play();
+        setPlayStaus(true);
+    }
+
+    const previous = async () =>{
+        if (isTrack.id > 0) {
+            const track_id = await setTrack(songsData[isTrack.id + 1])
+            const audio_ref = await audioRef.current.play();
+            setPlayStaus(true);
+        }
+    }
+
+    const next = async () =>{
+        if (isTrack.id < songsData.length - 1) {
+            const track_id = await setTrack(songsData[isTrack.id + 1])
+            const audio_ref = await audioRef.current.play();
+            setPlayStaus(true);
+        }
+    }
+
+    const seekSong = async (e) =>{
+        // console.log(e);
+        return audioRef.current.currentTime = ((e.nativeEvent.offsetX / seekBg.current.offsetWidth) * audioRef.current.duration);
+    }
     
-    const context_Value = {
+    const context_Value = { // 2. Creazione di una costante con tutte le possibili materiali da esportare.
         audioRef,
         seekBg,
         seekBar,
         isTrack, setTrack,
         playStatus,setPlayStaus,
         isTime, setTime,
-        play, pause
+        play, pause, playWithId, 
+        previous, next, seekSong
     }
 
+    // 3. Definire un attributo di valore con la variabile da esportare con i possibili materiali da aggiungere ai componenti.
   return (
     <PlayerContext.Provider value={context_Value}>
         {props.children}
